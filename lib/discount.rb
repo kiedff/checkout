917cea492756bf33be0ae98e5cd4f170db260e9f
kiedff <keith@keithmart.in>
2021-08-24 16:55:03 +0100

    fixes typo

--- lib/discount.rb
+++ lib/discount.rb
@@ -5,7 +5,7 @@ class Discount
       apple: { promotion: :two_for_one, one_per_customer: false },
       pear: { promotion: :two_for_one, one_per_customer: false },
       banana: { promotion: :half_price, one_per_customer: false },
-      banana: { promotion: :half_price, one_per_customer: true },
+      pineapple: { promotion: :half_price, one_per_customer: true },
       mangos: { promotion: :four_for_three, one_per_customer: false },
     }
   end
